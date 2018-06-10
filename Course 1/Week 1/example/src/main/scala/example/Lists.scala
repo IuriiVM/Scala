42c0548ceaf7dc737cba53dbade7490881e447f2
Iurii Maistrenko <imaistrenko@hotmail.com>
2018-06-10 22:00:39 +0200

    Example Assignment partially implemented

--- Course 1/Week 1/example/src/main/scala/example/Lists.scala
+++ Course 1/Week 1/example/src/main/scala/example/Lists.scala
@@ -1,5 +1,7 @@
 package example
 
+import java.util.NoSuchElementException
+
 
 object Lists {
 
@@ -23,7 +25,15 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-    def sum(xs: List[Int]): Int = ???
+    def sum(xs: List[Int]): Int = {
+      if (xs.isEmpty)
+        throw new Exception("List is empty")
+      else
+        if (xs.tail.isEmpty)
+          xs.head
+        else
+          xs.head + sum(xs.tail)
+    }
   
   /**
    * This method returns the largest element in a list of integers. If the
@@ -38,5 +48,18 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-    def max(xs: List[Int]): Int = ???
+    def max(xs: List[Int]): Int = {
+      if (xs.isEmpty)
+        throw new NoSuchElementException("List is empty")
+      else
+        if (xs.tail.isEmpty)
+          xs.head
+        else {
+          val localMax: Int = max(xs.tail)
+          if (xs.head > localMax)
+            xs.head
+          else
+            localMax
+        }
+    }
   }
